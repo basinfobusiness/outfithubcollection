fc71528dde0081b5185303a2d6e43574c93ad3f5
infospot <infospot112@gmail.com>
2025-04-24 09:43:33 +0000

    Add files via upload

--- catalog.js
+++ catalog.js
@@ -104,6 +104,6 @@ onChildAdded(bizinfo, (data) =>{
 'use strict'
     const ohc_now = document.createElement("small");
     const catalogNow = document.getElementById("catalog-log");
-    catalog.addChildElement(ohc_now);
+    catalog.appendChildElement(ohc_now);
     ohc_now.textContent = "Outfit Hub Collection Frequency" 
 })();
