b6e7cbc67166ad9166feaff8f9940f4ee2b736a2
infospot <infospot112@gmail.com>
2025-04-26 19:32:39 +0000

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-//import { getAnalytics } from "https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-auth.js';
-import { getFirestore, setDoc, doc} from 'https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js';
+  import { initializeApp } from "https:/\/www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+  import { getAnalytics } from "https:/\/www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https:/\/www.gstatic.com/firebasejs/11.6.1/firebase-auth.js';
+  import { getFirestore, setDoc, doc} from 'https:/\/www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js';
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCPH1vhHu7qyMCqQsogV2epGp34pW-ct-M",
+    authDomain: "outfit-hub-collection-34232.firebaseapp.com",
+    projectId: "outfit-hub-collection-34232",
+    storageBucket: "outfit-hub-collection-34232.firebasestorage.app",
+    messagingSenderId: "1058398717171",
+    appId: "1:1058398717171:web:e11c2433991ff35664a386",
+    measurementId: "G-1QXNMQ4NYZ"
+  };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCPH1vhHu7qyMCqQsogV2epGp34pW-ct-M",
-  authDomain: "outfit-hub-collection-34232.firebaseapp.com",
-  projectId: "outfit-hub-collection-34232",
-  storageBucket: "outfit-hub-collection-34232.firebasestorage.app",
-  messagingSenderId: "1058398717171",
-  appId: "1:1058398717171:web:e11c2433991ff35664a386",
-  measurementId: "G-1QXNMQ4NYZ"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 const signup = document.getElementById("signup-btn");
 const regstatus = document.getElementById("reg-status");
@@ -52,7 +52,7 @@ signup.addEventListener('click',(event)=>{
     })
     .catch((error)=>{
         const errorCode = error.code;
-        if(errorCode == "auth/email-already-in-use"){
+        if(errorCode === "auth/email-already-in-use"){
             regstatus.textContent = "email already registered!";
         }else{
             regstatus.textContent = "unable to create user";
@@ -60,11 +60,3 @@ signup.addEventListener('click',(event)=>{
     });
 });
 
-function ShowPassword(){
-    let password = document.getElementById("floatingPassword");
-    if(password.type==="password"){
-        password.type = "text"
-    }else{
-        password.type = "password";
-    }
-}            
