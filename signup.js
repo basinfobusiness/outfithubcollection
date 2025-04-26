749f872cc72c1969558cdcfa3e1ab98b1af561a3
infospot <infospot112@gmail.com>
2025-04-26 18:57:41 +0000

    Add files via upload

--- signup.js
+++ signup.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 //import { getAnalytics } from "https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-auth.js';
-import { getFirestore, setDoc, doc} from 'https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js'
+import { getFirestore, setDoc, doc} from 'https:/\/www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCPH1vhHu7qyMCqQsogV2epGp34pW-ct-M",
@@ -24,27 +24,27 @@ const regstatus = document.getElementById("reg-status");
 signup.addEventListener('click',(event)=>{
     event.preventDefault();
     
-    const fullname = document.getElementById("floatingFullname").value;
-    const phonenumber = document.getElementById("FloatingTelephone").value;
-    const email = document.getElementById("FloatingEmail").value;
-    const password = document.getElementById("FloatingPassword").value;
+    const _fullname = document.getElementById("floatingFullname").value;
+    const _phonenumber = document.getElementById("floatingTelephone").value;
+    const _email = document.getElementById("floatingEmail").value;
+    const _password = document.getElementById("floatingPassword").value;
     
     const auth = getAuth();
     const db = getFirestore();
     
-    createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(auth, _email, _password)
     .then((userCredential)=>{
         const user = userCredential.user;
         const userData = {
-            fullname: fullname,
-            telephonenumber: phonenumber,
-            email: email
+            fullname: _fullname,
+            telephonenumber: _phonenumber,
+            email: _email
         };
         const docRef = doc(db, "users", user.uid);
         regstatus.textContent = "Account successfully created!";
         setDoc(docRef,userData)
         .then(()=>{
-            window.location.assign("https:/\/www.outfithubcollection.com/login.html");
+            window.location.href = "https:/\/www.outfithubcollection.com/login.html";
         })
         .catch((error)=>{
             console.error("error writing userdata", error);
@@ -59,3 +59,12 @@ signup.addEventListener('click',(event)=>{
         }
     });
 });
+
+function ShowPassword(){
+    let password = document.getElementById("floatingPassword");
+    if(password.type==="password"){
+        password.type = "text"
+    }else{
+        password.type = "password";
+    }
+}            
