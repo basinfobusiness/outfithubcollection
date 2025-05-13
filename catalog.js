6129e22b51d0643b84546e13ac28e498dc521a78
infospot <infospot112@gmail.com>
2025-05-13 12:40:58 -0700

    refactor

--- catalog.js
+++ catalog.js
@@ -27,84 +27,78 @@
       
       /* retrieval section */
       if(data.val().productname !== ""){
-      let divData = "<div class='row'><div class='col-sm-6 col-md-4 col-lg-3'><div class='box'>" +
-           "<a href='https:/\/www.outfithubcollection.com/app.html'>" + 
-              "<div class='img-box'>" +
-                "<img src='https:/\/www.outfithubcollection.com/OHC.png'' alt=''>" +
-              "</div>" +
-              "<div class='detail-box'>" +
-                "<h6 id='product-name-2'>" +
-                  data.val().productname +
-               "</h6>" +
-               "<h6> Price<span> " +
-                    "¢" + data.val().productprice +
-                  "</span>" +
-                "</h6>" +
-              "</div>" +
-              "<div class='new'>" +
-                "<span>" +
-                  "New" +
-                "</span>" +
-              "</div>" +
-           " </a>" +
+      let divData = "<div class='col'>" + 
+          "<div class='p-3' style='background: #eeeeee;' data-bs-toggle='modal' data-bs-target='#view-product'>" +
+            // "<br>" + "<span class='text-capitalize rounded-circle p-3' style='background: white;'>new</span>" +
+            "<center><img src='https://www.outfithubcollection.com/ohc_merch.png' alt='' class='d-block' style='width:80%'></center>" +
+                 "<p class='mt-3 mb-2 fw-bold'>" + data.val().productname +
+               "<span class='float-end'>Price <span style='color: #db4f66;'>&#x20B5;" +
+                    data.val().productprice +
+                  "</span></span></p>" +
+            "</div>" +  
           "</div>" +
-        "</div></div></div><br>";
+      "</div>";
         
         if(typeof(Storage)!== "undefined"){
             localStorage.productname = data.val().productname;
         }
       let cataloglog = document.getElementById("catalog-log");
-          cataloglog.insertAdjacentHTML('beforebegin',divData);
+          cataloglog.insertAdjacentHTML('afterbegin',divData);
       console.log("i got loaded!");
       }
   });
   
   onChildAdded(bizrecords, (data) =>{
-   let bizData = "<div class='col-md-6 col-lg-3'><h6>" +
-              "CONTACT US" +
-            "</h6>" +
-           " <div class='info_link-box'><a href=''>" +
-               "<i class='fa " + "fa-map-marker' aria-hidden='true'></i> " +
-            "<span>" + data.val().businessaddress + "</span>" +
-            "</a>" +
-              "<a href=''>" +
-                "<i class='fa" + " fa-phone' aria-hidden='true'></i>" +
-                " <span>" + data.val().businessnumber + "</span>" +
-              "</a>" +
-              "<a href=''>" +
-                "<i class='fa" + " fa-envelope' aria-hidden='true'></i>" +
-               " <span>" + data.val().businessemail + "</span>" +
-              "</a></div></div>";
+   let bizData = "<a href='' class='text-decoration-none text-white'>" +
+                      "<i class='bi bi-geo-alt-fill'></i>" +
+                      "<span> " + data.val().businessaddress + "</span>" +
+                  "</a><br>" +
+              "<a href='tel:+233500518387' class='text-decoration-none text-white'>" +
+                "<i class='bi bi-telephone'></i>" +
+                  "<span> " + data.val().businessnumber + "</span>" +
+                  "</a><br>" +
+              "<a href='mailto:info@outfithubcollection' class='text-decoration-none text-white'>" +
+                "<i class='bi bi-envelope'></i>" +
+                  "<span> " + data.val().businessemail + "</span>" +
+                  "</a>";
 
 let businessdetails = document.getElementById("contact-details");
- businessdetails.insertAdjacentHTML('beforebegin',bizData);
+ businessdetails.insertAdjacentHTML('beforeend',bizData);
       console.log("i got loaded!");
       
   });
   
   /* About us */
 onChildAdded(bizinfo, (data) =>{
-    let infoData ="<div class='col-md-6 col-lg-3'>" +
-           "<h6>" +
-             "ABOUT US" +
-            "</h6>" +
-            "<p>" +
+    let infoData = "<p>" +
               data.val().aboutus +
-           "</p>" +
-          "</div>";
+           "</p>";
           
           let businessabout = document.getElementById("about-us");
-          businessabout.insertAdjacentHTML('beforebegin',infoData);
+          businessabout.insertAdjacentHTML('beforeend',infoData);
           
       console.log("i got loaded!");
 });
 
+// help and support
+onChildAdded(bizinfo, (data) =>{
+         
+ let helpData = "<p>" +
+              data.val().support + 
+            "</p>";
+          
+          let businesssupport = document.getElementById("help-and-support");
+          businesssupport.insertAdjacentHTML('beforeend',helpData);
+          
+      console.log("help and support!");
+});
+
 /* pod */
-(()=>{
-'use strict'
-    const ohc_now = document.createElement("small");
-    const catalogNow = document.getElementById("catalog-log");
-    catalogNow.appendChild(ohc_now);
- //   ohc_now.textContent = "<center>"+"Outfit Hub Collection Frequency"+"</center>";
- console.log("maintenance");
-})();
+// (()=>{
+// 'use strict'
+//     const ohc_now = document.createElement("small");
+//     const catalogNow = document.getElementById("catalog-log");
+//     catalogNow.appendChild(ohc_now);
+//  //   ohc_now.textContent = "<center>"+"Outfit Hub Collection Frequency"+"</center>";
+//  console.log("maintenance");
+// })();
